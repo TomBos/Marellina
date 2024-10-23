d736f2d569459dc6086f37874bd04e9482ffac7d
Tomáš Bosák <tombos255@gmail.com>
2024-10-24 00:07:58 +0200

    Image, css debuged

--- src/views/landingPage.jsx
+++ src/views/landingPage.jsx
@@ -8,9 +8,21 @@ const LandingPage = ({ stringObject }) => {
 
     return (
         <>
-            <h1 className='landing-page-title' >{stringObject.Title}</h1>
+
+            <div className='mobile'>
+                <svg viewBox="0 0 500 150">
+                  <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+                  <text width="500">
+                    <textPath alignmentBaseline='top' xlinkHref="#curve">
+                        {stringObject.Title}
+                    </textPath>
+                  </text>
+                </svg>
+                <img className='landing-page-img' src={stringObject.Image} alt="placeholder" />
+            </div>
+
         
-            <img src={stringObject.Image} alt="placeholder" />
+            
             <p>{stringObject.About}</p>
             <h1>{stringObject.CallToActionTitle}</h1>
 
