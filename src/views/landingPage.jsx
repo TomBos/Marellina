6a1a655f4dc7248b963c01d3d8d63b26e0547f03
Tomáš Bosák <tombos255@gmail.com>
2024-10-23 19:54:27 +0200

    Languange modification, Landing Page Template

--- src/views/landingPage.jsx
+++ src/views/landingPage.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
 
 const LandingPage = ({ stringObject }) => {
+
+    console.log(stringObject);
+
     return (
         <>
-            <h1>best goods from Hearth of Italy</h1>
+            <h1>{stringObject.Title}</h1>
 
+            <img src={stringObject.Image} alt="placeholder" />
+            <p>{stringObject.About}</p>
+            <h1>{stringObject.CallToActionTitle}</h1>
 
-            <img src="https://newmansown.com/wp-content/uploads/2022/04/evoooil.png" alt="placeholder" />
+            <ul>
+                {stringObject.Products && Object.keys(stringObject.Products).map((key, index) => {
+                    return  (
+                    <li key={index}>
+                        {stringObject.Products[key]}
+                    </li>
+                    );
+                })}
+            </ul>
+            
+            <h1>{stringObject.CallToActionReasoningTitle}</h1>
 
-            <p>   Indulge in the finest flavors of Italy, delivered straight to your door. From artisanal pasta and extra virgin olive oil to mouth-watering cheeses, sauces, and sweet treats, we bring you the best of Italian cuisine.</p>
-
-            <h1>Discover Our Selection:</h1>
-
-            <li>Freshly Made Pasta </li>
-            <li>Premium Olive Oils </li>
-            <li>Gourmet Cheeses    </li>
-            <li>Traditional Sauces </li>
-            <li>Delicious Desserts </li>
-
-            <h1>Why Choose Us?</h1>
-
-
-            <li>Authentic Products</li>
-            <li>Fast & Fresh Delivery</li>
-            <li>Curated Collections</li>
-
+            <ul>
+                {stringObject.ReasonsToBuy && Object.keys(stringObject.ReasonsToBuy).map((key, index) => {
+                    return  (
+                    <li key={index}>
+                        {stringObject.ReasonsToBuy[key]}
+                    </li>
+                    );
+                })}
+            </ul>
         </>
     );
 };
