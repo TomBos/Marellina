9d06c25a6a93211ba6ee427ec6e4927384ca6286
Tomáš Bosák <tombos255@gmail.com>
2024-11-19 00:33:40 +0100

    React Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,26 @@
-import './styles/system/main.scss';
-// === END OF STYLES ===
-
-import Navbar from './templates/navbar';
-import LandingPage from './views/landingPage';
-import React, { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
+import Home from './sites/home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // === END OF IMPORTS ===
 
 
 
-function App({ DEVMODE }) {
-  const [selectedLanguage, setSelectedLanguage] = useState({});
-  const [languageCode, setLanguageCode] = useState(Cookies.get('language') || 'cz'); // Load from cookie or default to 'cz'
+function App({DEVMODE}) {
 
   if (DEVMODE) {
     import('./styles/system/debug.scss');
   }
 
-  // Load the language JSON file based on languageCode
-  const loadLanguage = async (code) => {
-    try {
-      const response = await fetch(`./locales/${code}.json`);
-      const languageData = await response.json();
-      setSelectedLanguage(languageData);
-    } catch (error) {
-      console.error("Error loading language file:", error);
-    }
-  };
-
-  // Change language and load the respective JSON file
-  const changeLanguage = (code) => {
-    setLanguageCode(code);
-    Cookies.set('language', code, { expires: 365 });
-    loadLanguage(code);
-  };
-
-  // Load the initial language on mount
-  useEffect(() => {
-    loadLanguage(languageCode);
-  }, []); // Run once on mount
-
   return (
-    <>
-      <Navbar stringObject={selectedLanguage.Navbar || {}} changeLanguage={changeLanguage} />
-      <LandingPage stringObject={selectedLanguage.LandingPage || {}} />
-    </>
+      <>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+
+              <Route path="*" element="404 - Page not found" />
+              <Route path="/ctf" element="What are you doing here ?" />
+            </Routes>
+          </Router>
+      </>
   );
 }
 
--- src/views/landingPage.jsx
+++ src/views/landingPage.jsx
@@ -24,19 +24,13 @@ const LandingPage = ({ stringObject }) => {
         
             
             <p className="about">{stringObject.About}</p>
-            <h1>{stringObject.CallToActionTitle}</h1>
 
-            <ul>
-                {stringObject.Products && Object.keys(stringObject.Products).map((key, index) => {
-                    return  (
-                    <li key={index}>
-                        {stringObject.Products[key]}
-                    </li>
-                    );
-                })}
-            </ul>
+
+
             
             <h1>{stringObject.CallToActionReasoningTitle}</h1>
+
+            
 
             <ul>
                 {stringObject.ReasonsToBuy && Object.keys(stringObject.ReasonsToBuy).map((key, index) => {
