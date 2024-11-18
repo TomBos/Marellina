a28100a0fdb438293fa62dcb86ca63a7663c2afe
Tomáš Bosák <tombos255@gmail.com>
2024-11-19 00:13:33 +0100

    Oprava stylu, start mobilni verze

--- src/views/landingPage.jsx
+++ src/views/landingPage.jsx
@@ -9,7 +9,7 @@ const LandingPage = ({ stringObject }) => {
     return (
         <>
 
-            <div className='mobile'>
+            <div className='hero mobile'>
                 <svg viewBox="0 0 500 150">
                   <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
                   <text width="500">
@@ -23,7 +23,7 @@ const LandingPage = ({ stringObject }) => {
 
         
             
-            <p>{stringObject.About}</p>
+            <p className="about">{stringObject.About}</p>
             <h1>{stringObject.CallToActionTitle}</h1>
 
             <ul>
